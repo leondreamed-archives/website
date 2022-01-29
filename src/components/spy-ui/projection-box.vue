7e3ad484a8be457d63fd7a0838f44dd8c14b81d6
Leon Si <contact@leonzalion.com>
2022-01-29 16:19:16 -0500

    fix: positioning of projection

--- src/components/spy-ui/projection-box.vue
+++ src/components/spy-ui/projection-box.vue
@@ -2,15 +2,22 @@
 import { ref, computed } from 'vue';
 import TransparentVideo from '../transparent-video.vue';
 import { ComponentType } from '~/types/component';
-import { useInnerWindowSize } from '~/utils/window';
-import { torontoLocationX, torontoLocationY } from '~/utils/world-map';
+import {
+	torontoLocationX,
+	torontoLocationY,
+	worldMapHeight,
+	worldMapWidth,
+} from '~/utils/world-map';
 
-const { height, width } = useInnerWindowSize();
-const animationWidth = computed(() => width.value / 2);
-const animationHeight = computed(() => height.value / 2);
+const animationWidth = computed(() => worldMapWidth.value / 2);
+const animationHeight = computed(() => worldMapHeight.value / 2);
 
-const animationX = computed(() => torontoLocationX.value - 160.5);
-const animationY = computed(() => torontoLocationY.value - 81);
+const animationX = computed(
+	() => torontoLocationX.value - animationWidth.value * 0.213
+);
+const animationY = computed(
+	() => torontoLocationY.value - animationHeight.value * 0.21
+);
 
 const animationStarted = ref(false);
 const projectionVideo = ref<ComponentType<typeof TransparentVideo>>();
