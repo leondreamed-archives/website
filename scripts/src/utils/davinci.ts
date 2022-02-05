ad3b8037d683a2c598f51bb4fc920ed25f55ac46
Leon Si <contact@leonzalion.com>
2022-02-04 20:40:44 -0500

    refactor: move

---
+++ scripts/src/utils/davinci.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+import process from 'node:process';
+import type { Buffer } from 'node:buffer';
+import { execaSync, execa } from 'execa';
+
+type RunDavinciScriptProps = {
+	scriptPath: string;
+	envVars: Record<string, string>;
+};
+export async function runDavinciScript({
+	scriptPath,
+	envVars,
+}: RunDavinciScriptProps) {
+	const resolveExecutablePath =
+		'/Applications/DaVinci Resolve/DaVinci Resolve.app/Contents/MacOS/Resolve';
+
+	// Using the `script` executable to trick DaVinci into thinking we're
+	// running the command from a TTY (because DaVinci changes its output)
+	// otherwise that hides when the FusionScript Server starts which we
+	// need to know so that we know when to run our DaVinci script
+	const davinciProcess = execa('script', [
+		'-q',
+		'/dev/null',
+		resolveExecutablePath,
+		'-nogui',
+	]);
+
+	console.info('Waiting for the FusionScript server to start...');
+
+	// If the davinciProcess closed, that means the Fusion server has already started
+	const davinciProcessPid = await new Promise<number | undefined>((resolve) => {
+		void davinciProcess.on('close', () => {
+			resolve(undefined);
+		});
+
+		davinciProcess.stdout?.on('data', (data: Buffer) => {
+			const dataString = data.toString();
+			const result = /Host 'Fusion' \[(\d+)] Added/.exec(dataString);
+			if (result !== null) {
+				resolve(Number(result[1]));
+			}
+		});
+	});
+
+	const fuscriptPath =
+		'/Applications/DaVinci Resolve/DaVinci Resolve.app/Contents/Libraries/Fusion/fuscript';
+
+	console.info('Running the DaVinci script...');
+
+	execaSync(fuscriptPath, [scriptPath], {
+		env: {
+			PYTHONPATH: `${process.env
+				.PYTHONPATH!}:/Library/Application Support/Blackmagic Design/DaVinci Resolve/Developer/Scripting/Modules`,
+			...envVars,
+		},
+		stdio: 'inherit',
+	});
+
+	// Don't kill the Fusion server if it was already started before this script was run
+	if (davinciProcessPid !== undefined) {
+		process.kill(davinciProcessPid);
+	}
+}
