ad7ca2dc6b9884c8497b0b1b3c4107606fa6306f
Leon Si <contact@leonzalion.com>
2022-02-05 12:55:12 -0500

    fix: davinci scripts

--- scripts/src/davinci/python/render-compositions.py
+++ scripts/src/davinci/python/render-compositions.py
@@ -2,6 +2,7 @@ import DaVinciResolveScript as dvr_script
 from inspect import getsourcefile
 from os.path import abspath
 import os
+import json
 
 resolve = dvr_script.scriptapp("Resolve")
 fusion = resolve.Fusion()
@@ -10,7 +11,9 @@ dirname = os.path.dirname(abspath(getsourcefile(lambda:0)))
 rootPath = os.path.join(dirname, '../../../..')
 
 rendersFolder = os.path.normpath(os.path.join(rootPath, './assets/davinci/composition-renders'))
+
 projectName = os.environ['PROJECT_NAME']
+compositionNamesToRender = json.loads(os.environ['COMPOSITION_NAMES_TO_RENDER'])
 
 project = projectManager.LoadProject(projectName)
 
@@ -21,8 +24,11 @@ timelineItems = timeline.GetItemListInTrack("video", 1)
 project.DeleteAllRenderJobs()
 
 project.SetCurrentRenderFormatAndCodec("mov", "ProRes444")
+
 for timelineItem in timelineItems:
 	name = timelineItem.GetName()
+	if name not in compositionNamesToRender: continue
+
 	start = timelineItem.GetStart()
 	# Subtracting 1 from the end or otherwise it renders a frame of the next clip
 	end = timelineItem.GetEnd() - 1
@@ -36,3 +42,18 @@ for timelineItem in timelineItems:
 	project.AddRenderJob()
 
 project.StartRendering()
+
+import time
+
+# Waiting until all projects finish rendering (TODO: print render job status using project.GetRenderJobStatus(idx))
+
+def waitForRenders():
+	mustend = time.time() + 180 # 3 minute timeout
+	while time.time() < mustend:
+		if not project.IsRenderingInProgress(): return True
+		time.sleep(0.25)
+	return False
+
+if not waitForRenders():
+	raise Exception("Rendering timeout exceeded.")
+
