3703b58cc1212b212297d03105042ded50b4c762
Leon Si <contact@leonzalion.com>
2022-02-07 18:58:03 -0500

    feat: use s3 in production

--- frontend/src/components/transparent-video.vue
+++ frontend/src/components/transparent-video.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-defineProps<{ videoName: string }>();
+const props = defineProps<{ videoName: string }>();
 const video = ref<HTMLVideoElement>();
+
+const isProduction = import.meta.env.PROD;
+const hevcVideoSrc = isProduction
+	? `https://leonzalion-website-files.s3.us-east-1.amazonaws.com/videos/${props.videoName}-hevc.mp4`
+	: `videos/${props.videoName}-hevc.mp4`;
+
+const webmVideoSrc = isProduction
+	? `https://leonzalion-website-files.s3.us-east-1.amazonaws.com/videos/${props.videoName}-webm.webm`
+	: `videos/${props.videoName}-webm.webm`;
 
 defineExpose({
 	video,
@@ -11,13 +20,7 @@ defineExpose({
 
 <template>
 	<video ref="video" muted>
-		<source
-			:src="`videos/${videoName}-hevc.mp4`"
-			type='video/mp4; codecs="hvc1"'
-		/>
-		<source
-			:src="`videos/${videoName}-webm.webm`"
-			type="video/webm"
-		/>
+		<source :src="hevcVideoSrc" type='video/mp4; codecs="hvc1"' />
+		<source :src="webmVideoSrc" type="video/webm" />
 	</video>
 </template>
