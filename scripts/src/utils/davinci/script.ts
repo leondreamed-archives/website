a61c20a93897e6285131fe3f4e21c8a2e43e049a
Leon Si <contact@leonzalion.com>
2022-03-20 19:40:55 -0400

    fix: silent

--- scripts/src/utils/davinci/script.ts
+++ scripts/src/utils/davinci/script.ts
@@ -27,7 +27,9 @@ export async function runDavinciScript({
 	const fuscriptPath =
 		'/Applications/DaVinci Resolve/DaVinci Resolve.app/Contents/Libraries/Fusion/fuscript';
 
-	logDebug(() => 'Running the DaVinci script...');
+	if (!silent) {
+		logDebug(() => 'Running the DaVinci script...');
+	}
 
 	const scriptProcess = execa(
 		'script',
